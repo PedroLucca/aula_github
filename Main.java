bbfd1f7b4252191d21f40e98ac34c35b1c9c6369
mizaelgoonfs <miza14pg@hotmail.com>
2021-05-08 13:36:47 -0300

    Implementação do cadastro de cliente #1

--- Main.java
+++ Main.java
@@ -1,20 +1,27 @@
 import java.util.Arrays;
+import funcionalidades.CadastrarCliente;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Sair", "Cadastrar Cliente", "Verificar Saldo", "Realizar Saque", "Realizar Deposito", "Verificar Extrato"));
 		//System.out.println(mainMenu.getSelection() + "foi selecionada");
-		switch(mainMenu.getSelection()){
-			case 1:
-				System.out.println("Saiu com sucesso");
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Opção invalida");
-
+		while(true){
+			switch(mainMenu.getSelection()){
+				case 1:
+					System.out.println("Saiu com sucesso");
+					System.exit(0);
+					break;
+				case 2:
+					CadastrarCliente cadastro =  new CadastrarCliente();
+					cadastro.cadastrar_cliente();
+					break;
+				default:
+					break;
+			}
+			//System.out.println("Fim");
 		}
-		//System.out.println("Fim");
 	}
 
 }
---
+++ classes/Cliente.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class Cliente {
+	private String nome;
+	private String cpf;
+
+	public Cliente(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+		
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+		
+	public void setQuantidade(String cpf) {
+		this.cpf = cpf;
+	}
+}

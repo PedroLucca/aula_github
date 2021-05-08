4de4e2afd8e4a7596a3fca6f18ac8c5a91974e4d
NaumCS <naum_celestino@hotmail.com>
2021-05-08 12:56:34 -0300

    implementação deposito #4

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
-		System.out.println("Fim");
+		
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Sair", "Cadastrar Cliente", "Verificar Saldo", "Realizar Saque", "Realizar Deposito", "Verificar Extrato"));
+		//System.out.println(mainMenu.getSelection() + "foi selecionada");
+		switch(mainMenu.getSelection()){
+			case 1:
+				System.out.println("Saiu com sucesso");
+				System.exit(0);
+				break;
+			case 5:
+				Deposito novo = new Deposito(50);
+			default:
+				System.out.println("Opção invalida");
+
+		}
+		//System.out.println("Fim");
 	}
 
 }
